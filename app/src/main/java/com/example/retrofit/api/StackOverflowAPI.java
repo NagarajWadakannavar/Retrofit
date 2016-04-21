fa205ec03b181fe140e69ecf0527b8d7b81dbcbb
NAGARAJ W <nagarajw@ndtv.com>
2016-04-21 12:11:20 +0530

    Added the different ways of constructing the request.

--- app/src/main/java/com/example/retrofit/api/StackOverflowAPI.java
+++ app/src/main/java/com/example/retrofit/api/StackOverflowAPI.java
@@ -2,8 +2,14 @@ package com.example.retrofit.api;
 
 import com.example.retrofit.model.Questions;
 
+import java.util.Map;
+
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+import retrofit2.http.QueryMap;
+import retrofit2.http.Url;
 
 /**
  * Created by NAGARAJ on 4/18/2016.
@@ -11,4 +17,15 @@ import retrofit2.http.GET;
 public interface StackOverflowAPI {
     @GET("/2.2/questions?order=desc&sort=creation&site=stackoverflow")
     Call<Questions> downloadQuestions();
+
+    @GET
+    Call<Questions> downloadQuestions(@Url String url);
+
+    @GET("/2.2/{type}?order=desc&sort=creation")
+    Call<Questions> downloadQuestions(@Path("type") String user,@Query("site") String query);
+
+    @GET("/2.2/questions")
+    Call<Questions> downloadQuestions(@QueryMap Map<String, String> options);
+
+
 }
--- app/src/main/java/com/example/retrofit/ui/QuestionListFragment.java
+++ app/src/main/java/com/example/retrofit/ui/QuestionListFragment.java
@@ -7,11 +7,18 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
 import android.widget.ProgressBar;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import com.example.retrofit.R;
 import com.example.retrofit.api.StackOverflowAPI;
 import com.example.retrofit.model.Questions;
 import com.example.retrofit.ui.adapter.ListAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -22,10 +29,12 @@ import retrofit2.converter.gson.GsonConverterFactory;
 /**
  * A placeholder fragment containing a simple view.
  */
-public class QuestionListFragment extends Fragment {
+public class QuestionListFragment extends Fragment implements RadioGroup.OnCheckedChangeListener {
 
     private ListView listView;
     private ProgressBar progressBar;
+    private StackOverflowAPI api;
+    private List<Questions.Question> questionList = new ArrayList<>();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -33,6 +42,7 @@ public class QuestionListFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_main, container, false);
         listView = (ListView) view.findViewById(R.id.list_view);
         progressBar = (ProgressBar) view.findViewById(R.id.progress_bar);
+
         return view;
     }
 
@@ -40,14 +50,24 @@ public class QuestionListFragment extends Fragment {
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         Retrofit retrofit = new Retrofit.Builder().baseUrl("https://api.stackexchange.com").addConverterFactory(GsonConverterFactory.create()).build();
-        StackOverflowAPI api = retrofit.create(StackOverflowAPI.class);
-        Call<Questions> questionsCall = api.downloadQuestions();
+        api = retrofit.create(StackOverflowAPI.class);
+        listView.setAdapter(new ListAdapter(getActivity(), questionList));
+        RadioGroup radioGroup = (RadioGroup) getView().findViewById(R.id.radio_group);
+        radioGroup.setOnCheckedChangeListener(this);
+        RadioButton simpleRequest = (RadioButton) radioGroup.findViewById(R.id.request);
+        simpleRequest.setChecked(true);
+
+    }
+
+    private void downloadQuestions(Call<Questions> questionsCall) {
+        progressBar.setVisibility(View.VISIBLE);
         questionsCall.enqueue(new Callback<Questions>() {
             @Override
             public void onResponse(Call<Questions> call, Response<Questions> response) {
                 if (getActivity() == null)
                     return;
-                listView.setAdapter(new ListAdapter(getActivity(),response.body().items));
+                questionList.addAll(response.body().items);
+                ((ListAdapter) listView.getAdapter()).notifyDataSetChanged();
                 progressBar.setVisibility(View.GONE);
 
             }
@@ -59,5 +79,33 @@ public class QuestionListFragment extends Fragment {
                 progressBar.setVisibility(View.GONE);
             }
         });
+    }
+
+    @Override
+    public void onCheckedChanged(RadioGroup group, int checkedId) {
+        questionList.clear();
+
+        ((ListAdapter) listView.getAdapter()).notifyDataSetChanged();
+        Call<Questions> questionsCall = null;
+        switch (group.getCheckedRadioButtonId()) {
+            case R.id.request:
+                questionsCall = api.downloadQuestions();
+                break;
+            case R.id.dynamic_url:
+                questionsCall = api.downloadQuestions("https://api.stackexchange.com/2.2/questions?order=desc&sort=creation&site=stackoverflow");
+                break;
+            case R.id.url_manipulation:
+                questionsCall = api.downloadQuestions("questions", "stackoverflow");
+                break;
+            case R.id.query_map:
+                Map<String, String> map = new HashMap<>();
+                map.put("site", "stackoverflow");
+                map.put("sort", "creation");
+                map.put("order", "desc");
+                questionsCall = api.downloadQuestions(map);
+                break;
+        }
+        downloadQuestions(questionsCall);
+
     }
 }
